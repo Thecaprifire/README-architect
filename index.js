322b2363725fc85508ece3faacadd73c03e653a2
Ian Daluz <janiandaluz@yahoo.com>
2024-06-12 07:54:44 -0600

    added comments to index.js

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const { prompt } = require('inquirer');
 const { writeFile } = require('fs');
 const { promisify } = require('util');
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = require('./library/questions');
 
-// TODO: Create a template
+// Import the template generator function
 const generateREADME = require('./library/template');
 
-// TODO: Create a function to write README file
+// Promisify the writeFile function to use async/await syntax
 const writeFileAsync = promisify(writeFile);
 
+// Welcome message for the user
 console.log('Welcome to Github README generator!');
 
-// TODO: Create a function to prompt user questions
+// Function to prompt user with questions
 function promptUser() {
+    // Use inquirer to prompt questions and return the answers
     return prompt(questions);
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize the app
 async function init() {
     try {
+        // Prompt the user and wait for answers
         const answers = await promptUser();
+
+        // Log the answers for debugging purposes
         console.log(JSON.stringify(answers, null, '\t'));
 
+        // Generate the README content using the user's answers
         const readme = generateREADME(answers);
+
+        // Write the generated README content to a file
         await writeFileAsync('README.md', readme);
 
+        // Log success message
         console.log('Successfully wrote to README.md');
     } catch (err) {
+        // Log any errors that occur during the process
         console.error(err);
     }
 }
 
-// Function call to initialize app
+// Call the init function to start the app
 init();
