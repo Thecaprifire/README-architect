c06cc3fd108263fb58e9c57542add46b55a0b415
Ian Daluz <janiandaluz@yahoo.com>
2024-06-12 07:34:35 -0600

    added code to index.js

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 // TODO: Include packages needed for this application
+const { prompt } = require('inquirer');
+const { writeFile } = require('fs');
+const { promisify } = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = require('./library/questions');
+
+// TODO: Create a template
+const generateREADME = require('./library/template');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFileAsync = promisify(writeFile);
+
+console.log('Welcome to Github README generator!');
+
+// TODO: Create a function to prompt user questions
+function promptUser() {
+    return prompt(questions);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await promptUser();
+        console.log(JSON.stringify(answers, null, '\t'));
+
+        const readme = generateREADME(answers);
+        await writeFileAsync('README.md', readme);
+
+        console.log('Successfully wrote to README.md');
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 // Function call to initialize app
 init();
